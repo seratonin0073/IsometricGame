ecbe27282455acbfc3b2997a1f7092b92a64260b
Mizko Danylo <mizko2000y@gmail.com>
2024-02-10 19:56:29 +0200

    Lesson19 add animation

--- Assets/Script/AnimationControler.cs
+++ Assets/Script/AnimationControler.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static readonly string[] staticDirection =
     {
-        
+        "StaticN", "StaticNE", "StaticE","StaticSE", "StaticS", "StaticSW","StaticW", "StaticNW"
+    };
+
+    public static readonly string[] runDirection =
+    {
+        "RunN", "RunNE", "RunE","RunSE", "RunS", "RunSW","RunW", "RunNW"
+    };
+
+    Animator anim;
+    int lastDirection;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+    public void SetDirection(Vector2 direction)
+    {
+        string[] directionArray = null;
+
+        if (direction.magnitude < 0.01)
+        {
+            directionArray = staticDirection;
+        }
+        else
+        {
+            directionArray = runDirection;
+            lastDirection = DirectionToIndex(direction);
+        }
+        anim.Play(directionArray[lastDirection]);
+    }
+
+    private int DirectionToIndex(Vector2 direction)
+    {
+        Vector2 norDir = direction.normalized;
+
+        float step = 45;
+        float offset = step / 2;
+
+        float angel = Vector2.SignedAngle(Vector2.up, norDir);
+        angel += offset;
+        if(angel < 0)
+        {
+            angel += 360;
+        }
+
+        float stepCount = angel / step;
+        return Mathf.FloorToInt(stepCount);
+    }
+}
+
+enum StaticDirection
+{
+    StaticN, StaticNE, StaticE, StaticSE, StaticS, StaticSW, StaticW, StaticNW
+}
+
+enum RunDirection
+{
+    RunN, RunNE, RunE, RunSE, RunS, RunSW, RunW, RunNW
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private float moveH, moveV;
+
     [SerializeField] private float moveSpeed = 1f;
+
+    
+    
     
     void Awake()
     {
@@ -17,7 +21,12 @@ public class PlayerController : MonoBehaviour
     {
         moveH = Input.GetAxis("Horizontal") * moveSpeed;
         moveV = Input.GetAxis("Vertical") * moveSpeed;
-        rb2d.velocity = new Vector2(moveH, moveV);
+        Vector2 direction = new Vector2(moveH, moveV);
+        rb2d.velocity = direction;
+
+        FindObjectOfType<AnimationControler>().SetDirection(direction);
+
+        
     }
 
     void Update()
