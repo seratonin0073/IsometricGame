dcbf8b6137f8619eb19ed3545f6be78b2e75f183
Mizko Danylo <mizko2000y@gmail.com>
2024-03-16 19:40:39 +0200

    Add enemy and damage

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public Patrol patrol;
     public AIDestinationSetter destinator;
     public GameObject target;
+    public int health = 5;
+    public Slider hpBar;
+
+    void Start()
+    {
+        hpBar.maxValue = health;
+        hpBar.value = health;
+    }
 
     void Update()
     {
@@ -23,5 +32,31 @@ public class Enemy : MonoBehaviour
             patrol.enabled = true;
             destinator.enabled = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerSettings>().Damage(transform.position);
+        }
+        else if(collision.gameObject.CompareTag("Blast"))
+        {
+            health--;
+            hpBar.value = health;
+            Destroy(collision.gameObject);
+
+            transform.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+            Invoke("SetWhiteColor", 0.2f);
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void SetWhiteColor()
+    {
+        transform.GetComponentInChildren<SpriteRenderer>().color = Color.white;
     }
 }
---
+++ Assets/Script/PlayerSettings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSettings : MonoBehaviour
+{
+    private int health = 3;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Sprite[] healthImage;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        healthBar.sprite = healthImage[health];
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void Damage(Vector3 enemyPos)
+    {
+        health--;
+        Vector2 impulse = enemyPos - transform.position;
+        //GetComponent<Rigidbody2D>().AddForce(impulse * 100f, ForceMode2D.Impulse);
+        transform.Translate(-impulse * 3f);
+        //rb.AddForce(-impulse*100f, ForceMode2D.Impulse);
+        Debug.LogError(rb.name);
+        if(health <= 0)
+        {
+            Application.LoadLevel(0);
+        }
+        healthBar.sprite = healthImage[health];
+
+    }
+}
